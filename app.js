5ee4ee0279899d25e3c90d87d9258a0beba85d22
Arfa Adnan <arfaadnan0@gmail.com>
2023-09-25 08:52:32 +0500

    assigment05

---
+++ app.js
@@ -0,0 +1,383 @@
+// ************************Assigment 05**************************
+// ************************chapter21***********************************
+// Question no 1
+// var allLower = userInput.toLowerCase();
+
+// Question no 02
+// x = x.toLowerCase();
+
+// Question no 03
+// y = y.toUpperCase();
+
+// Question no 04
+// var originalString = "YourOriginalString";
+// var newString = originalString.toLowerCase();
+
+// Question no 05
+// var newArrayElement = myArray[0].toLowerCase();
+
+// Question no 06
+// var myString = "YourString";
+// alert(myString.toUpperCase());
+
+// Question no 07
+// var cityName = "kaRacHi";
+// cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
+
+// *******************************chapter no 22-25*****************************************
+
+// Question no 01
+// var sameWords = "captain";
+// var slicedPart = sameWords.slice(1, 3);
+
+// Question no 02
+// var myString = "example";
+// var stringLength = myString.length;
+
+// Question no 03
+// var animal = "elephant";
+// var seg = animal.slice(1, 5);
+
+// // Question no 04
+// var myString = "example";
+// var stringLength = myString.length;
+
+// Question no 05
+// var myString = "example";
+// var stringLength = myString.length;
+// var slicedString = myString.slice(1, -3);
+
+// Question no 06
+// The value of indx is 3 in this case.
+
+// Question no 07
+// The value of indx is 14 in this case.
+
+// Question no 08
+// var indx = text.lastIndexOf("go");
+
+// Question no 09
+// if (typeof yourString[indexNum] !== 'undefined') {
+//     // Segment exists at the specified index
+//   } else {
+//     // Segment doesn't exist at the specified index
+//   }
+  
+// Question no 10
+// The character at index 2 in the string "abcde" is "c".
+
+// Question no 11
+// var cha = text.charAt(9);
+
+// Question no 12
+// var x = str.charAt(str.length - 1);
+
+// Question no 13
+// var cha = input.charAt(4);
+
+// Question no 14
+// if (yourString.charAt(2) === 'yourCharacter') {
+//     // The 3rd character is the specified character
+//   } else {
+//     // The 3rd character is not the specified character
+//   }
+
+// Question no 15
+// var myString = "example";
+// var myArray = [];
+
+// for (var i = 0; i < myString.length; i++) {
+//   myArray[i] = myString.charAt(i);
+// }
+
+//   Question no 15
+// var revisedReply = reply.replace("no", "yes");
+
+// Question no 16
+// var newStr = str.replace("1", "one");
+
+// Question no 17
+// var y = x.replace(/a/g, "z");
+
+// ******************************Chapter no 27***************************
+// Question no 1
+// var roundedNumber = Math.round(yourNumber);
+
+// Question no 02
+// var roundNum = Math.ceil(origNum);
+
+// Question no 03
+// var roundNum = Math.floor(origNum);
+
+// // Question no 04
+// var originalNumber = 7.8; // Replace with your number
+// var roundedNumber = Math.round(originalNumber);
+
+// // Question no 05
+// var roundedValue = Math.round(0.5);
+
+
+// ************************chapter no 27**************************
+// Question no 01
+// var randomInRange = Math.floor(Math.random() * 50) + 1;
+
+// // Question no 02
+// var randomNumber = Math.random();
+
+// // Question no 03
+// var diceRoll = Math.floor(Math.random() * 6) + 1;
+
+// // Question no 04
+// var coinToss = Math.random() < 0.5 ? "Heads" : "Tails";
+
+// ***********************************chapter no 28-29*****************************
+
+// // Question no 01
+// To convert a string to an integer in JavaScript, you can use the parseInt() function or the Number() constructor.
+
+// // Question no 02
+// function stringToInt(str) {
+//     return parseInt(str, 10);
+//   }
+  
+//   var result = stringToInt("123"); // result will be 123
+  
+// // Question no 03
+// To convert a string containing a decimal number to a 
+// floating-point number in JavaScript, you can use the parseFloat() function or the Number() constructor.
+
+// // Question no 04
+// function isNumeric(str) {
+//     return !isNaN(parseFloat(str)) && isFinite(str);
+//   }
+  
+//   var str1 = "123";
+//   var str2 = "3.14";
+//   var str3 = "abc";
+  
+//   console.log(isNumeric(str1)); // true
+//   console.log(isNumeric(str2)); // true
+//   console.log(isNumeric(str3)); // false
+  
+// // Question no 05
+// To convert a number to a string in JavaScript,
+//  you can use the toString() method or concatenate it with an empty string.
+// // Question no 06
+// function numberToString(num) {
+//     return num.toString();
+//   }
+  
+//   var result = numberToString(42); // result will be "42"
+  
+// // Question no 07
+// var decimalString = "3.14";
+// var integerNumber = parseInt(decimalString, 10); // integerNumber will be 3
+
+// However, if you want to round to the nearest integer instead 
+// of truncating, you can use Math.round() in combination with parseFloat():
+
+
+// var decimalString = "3.14";
+// var roundedInteger = Math.round(parseFloat(decimalString)); // roundedInteger will be 3
+
+
+// *********************chapter no 30****************************
+// Question no 1
+// var num = 3.14159265359;
+// var newNum = num.toFixed(4).toString();
+
+// // Question no 2
+// var yourNumber = 5.6789;
+// yourNumber = parseFloat(yourNumber.toFixed(2));
+
+// // Question no 3
+// if (num.toFixed(2).toString().length > 4) {
+//     // Code to execute if the string representation has more than 4 characters
+//   } else {
+//     // Code to execute if the string representation has 4 or fewer characters
+//   }
+  
+// // Question no 4
+// var originalNumber = 123.456789;
+// var roundedNumberString = originalNumber.toFixed(2).toString();
+// alert(roundedNumberString);
+
+
+// **************************************the end******************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Question no 06
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
